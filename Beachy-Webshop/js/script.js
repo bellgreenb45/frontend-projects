3752241b42d1164855d4949408d7139adc61a2ea
Sarah Greenberg <68124710+bellgreenb@users.noreply.github.com>
2025-04-25 20:47:28 -0400

    separate header and footer to different files

--- Beachy-Webshop/js/script.js
+++ Beachy-Webshop/js/script.js
@@ -12,3 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+$('#header-include').load('https://bellgreenb45.github.io/Beachy-Webshop/header.html', function() {
+    console.log("Header loaded successfully"); // Debug: Confirm header load
+    const navbar = document.querySelector(".navbar");
+    const scroll = window.scrollY;
+    if (scroll > 300) {
+        navbar.style.background = "#333"; // Dark color
+        console.log("Background set to dark (#333) after header load"); // Debug: Confirm background change
+    } else {
+        navbar.style.background = "transparent"; // Light color (transparent)
+        console.log("Background set to light (transparent) after header load"); // Debug: Confirm background change
+    }
+});
+
+$('#footer-include').load('https://bellgreenb45.github.io/Beachy-Webshop/footer.html');
